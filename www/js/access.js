b93c677bbdae9b79a1f5e949cd254976ee4c9b11
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-03 11:43:48 -0500

    let

--- www/js/access.js
+++ www/js/access.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var loginStatus = sessionStorage.getItem('loggedin') ;
+    console.log(loginStatus)
     if(loginStatus != 'loggedIn' || loginStatus != 'loggedOut'){
         sessionStorage.setItem('loggedin',"");
         sessionStorage.setItem('data',"");
