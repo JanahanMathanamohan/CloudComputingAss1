3bd3882e549e882ae9f3cd8dac02a63a4ecf0806
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-03 15:28:37 -0500

    H

--- www/js/YelpSearch.js
+++ www/js/YelpSearch.js
@@ -22,15 +22,17 @@ $(document).ready(function(){
     });
     function fill(data){
         console.log(data.buisness);
+        $('#resultHide').empty();
         var item = data.buisness;
         var panel2 = '<div id=lD'+i+' class="panel panel-danger ps" ><div class="markerpoint panel-heading psh"><h3 class="panel-title psh"><b>Results</h3></div>';
         for(var i = 0; i < item.length;i++){
             var tmp = item[i];
-            panel2 += '<div class="panel-body bottomLineR" id="l'+i+'S'+x+'D">';
+            panel2 += '<div class="panel-body bottomLineR" id="l'+i+'SD">';
             panel2 += '<div  class="col-md-8">'+tmp.name+'<br>Rating: '+tmp.rating+'<br>'+tmp.snippet_text+'<br>'+tmp.location.address+'<br><a href="'+tmp.url+'">Link</a><br>'+tmp.SALARY+'</div><div class="col-md-4 text-center maxH"><button type="button">Interest</button></div></div>';
-            panel2 += '<div class="col-md-4 text-center maxH"><button type="button" id="l'+i+'S'+x+'" class="discardB btn btn-primary ">ADD</button></div></div>';
+            panel2 += '<div class="col-md-4 text-center maxH"><button type="button" id="l'+i+'S" class="discardB btn btn-primary ">ADD</button></div></div>';
         }
         panel2 += '</div>';
+        console.log(panel2);
         $('#resultHide').append(panel2);
     }
     function remove(){
