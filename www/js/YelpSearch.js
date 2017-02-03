07b151c31186e935cbaa382a10a82bd4e0c12949
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-03 15:46:01 -0500

    H

--- www/js/YelpSearch.js
+++ www/js/YelpSearch.js
@@ -25,12 +25,15 @@ $(document).ready(function(){
         var item = data.businesses;
         console.log(item);
         var panel2 = '<div id=lD class="panel panel-danger ps" ><div class="markerpoint panel-heading psh"><h3 class="panel-title psh"><b>Results</h3></div>';
+        panel2 += '<div class="panel-body bottomLineR" id="l'+i+'SD">';
+        panel2 += "<ul class='list-group'>";
         for(var i = 0; i < item.length;i++){
             var tmp = item[i];
-            panel2 += '<div class="panel-body bottomLineR" id="l'+i+'SD">';
-            panel2 += '<div  class="col-md-8">'+tmp.name+'<br>Rating: '+tmp.rating+'<br>'+tmp.snippet_text+'<br>'+tmp.location.address+'<br><a href="'+tmp.url+'">Link</a><br>'+tmp.SALARY+'</div><div class="col-md-4 text-center maxH"><button type="button">Interest</button></div></div>';
-            panel2 += '<div class="col-md-4 text-center maxH"><button type="button" id="l'+i+'S" class="discardB btn btn-primary ">ADD</button></div></div>';
+            console.log(tmp)
+            panel2 += '<li class="list-group-item">'+tmp.name+'<br>Rating: '+tmp.rating+'<br>'+tmp.snippet_text+'<br>'+tmp.location.address+'<br><a href="'+tmp.url+'">Link</a><br>'+tmp.SALARY+'</div><div class="col-md-4 text-center maxH"><button type="button">Interest</button></li>';
         }
+        panel2 += '</ul>'
+        panel2 += '</div>'
         panel2 += '</div>';
         console.log(panel2);
         $('#resultHide').append(panel2);
