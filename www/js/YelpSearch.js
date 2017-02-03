e9b983d837c236abec95686d3bae2f049b19d58f
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-03 15:23:55 -0500

    L

--- www/js/YelpSearch.js
+++ www/js/YelpSearch.js
@@ -12,12 +12,33 @@ $(document).ready(function(){
                 console.log("success")
                 console.log(data);
                 if(data.error){
-                    alert(data.message)
+                    alert(data.message.data)
+                    create
                 }else{
                     console.log(data.message);
                 }
             }
         },"json");
     });
+    function fill(data){
+        console.log(data.buisness);
+        $('#resultHide').remove();
+        var item = data.buisness;
+        var panel2 = '<div id=lD'+i+' class="panel panel-danger ps" ><div class="markerpoint panel-heading psh"><h3 class="panel-title psh"><b>Results</h3></div>';
+        for(var i = 0; i < item.length;i++){
+            var tmp = item[i];
+            panel2 += '<div class="panel-body bottomLineR" id="l'+i+'S'+x+'D">';
+            panel2 += '<div  class="col-md-8">'+tmp.name+'<br>Rating: '+tmp.rating+'<br>'+tmp.snippet_text+'<br>'+tmp.location.address+'<br><a href="'+tmp.url+'">Link</a><br>'+tmp.SALARY+'</div><div class="col-md-4 text-center maxH"><button type="button">Interest</button></div></div>';
+            panel2 += '<div class="col-md-4 text-center maxH"><button type="button" id="l'+i+'S'+x+'" class="discardB btn btn-primary ">ADD</button></div></div>';
+        }
+        panel2 += '</div>';
+        $('#resultHide').append(panel2);
+    }
+    function remove(){
+
+    }
+    function addFav(){
+
+    }
 });
 
