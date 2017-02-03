2010a55e64c25a7f96b0dd2c06e7133646eab10f
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-02 19:45:56 -0500

    let

--- www/js/SignIn.js
+++ www/js/SignIn.js
@@ -8,17 +8,14 @@ function onSignIn(googleUser) {
     $('#signOut').removeClass('hidden');
     $('#signOut').addClass('show');
 }
-
-
+function signOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        $('#signOut').removeClass('show');
+        $('#signOut').addClass('hidden');
+    });
+}
 $(document).ready(function(){
-    function signOut(){
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-            console.log('User signed out.');
-            $('#signOut').removeClass('show');
-            $('#signOut').addClass('hidden');
-        });
-    }
-
 });
 
