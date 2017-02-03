c25ddfc37211c452e8e197027c5b2014a5e33bbf
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-03 11:56:30 -0500

    let

--- www/js/access.js
+++ www/js/access.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     var loginStatus = localStorage.getItem('loggedin') ;
-    if(loginStatus != 'loggedIn' || loginStatus != 'loggedOut'){
-        localStorage.setItem('loggedin',"");
-        localStorage.setItem('data',"");
-        localStorage.setItem('prof',"");
-        console.log("Not Logged in");
+    if(loginStatus != 'loggedIn' ){
+        if(loginStatus != 'loggedOut'){
+            localStorage.setItem('loggedin',"loggedOut");
+            localStorage.setItem('data',"");
+            localStorage.setItem('prof',"");
+            console.log("Not Logged in");
+        }
+        window.location.href = "../index.html";
     }
 });
 
