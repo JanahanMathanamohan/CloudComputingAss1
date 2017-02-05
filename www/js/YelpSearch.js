1750a5b90dc3ffc239022ca03f2cb04676b57e1d
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-04 23:38:44 -0500

    H

--- www/js/YelpSearch.js
+++ www/js/YelpSearch.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
             var tmp = item[i];
             panel2 += '<li class="list-group-item" id=R'+i+'><img src='+tmp.image_url+' />'+tmp.name+'<br>Rating: '+tmp.rating+'<br>'+tmp.snippet_text+'<br>'+tmp.location.address+'<br><a href="'+tmp.url+'" target=_blank>Link</a><br><button type="button" onclick=interest('+i+','+tmp+')>Interest</button></li>';
         }
+        console.log(panel2);
         $('#results').append(panel2);
     }
     function interest(num, tmp){
