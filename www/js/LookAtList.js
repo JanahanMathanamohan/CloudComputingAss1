973e82c69d2d96e1fdb062aba4074ee4e9b8a784
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-05 17:14:27 -0500

    H

--- www/js/LookAtList.js
+++ www/js/LookAtList.js
@@ -1,30 +1,9 @@
 $(document).ready(function(){
     var count = 0;
     var order = 0;
-    var update = [];
-    var results;
-    $("#submit").click(function(){
-        var toSend = {
-            term: $("#term").val(),
-            location: $("#location").val(),
-            radius: $("#radius").val(),
-            sort : 0,
-            limit: $("#limit").val()
-        };
-        $.post("https://cloudcompyelp.herokuapp.com/api/yelpSearch",toSend,function(data,status){
-            if(status == "success"){
-                console.log("success")
-                console.log(data);
-                if(data.error){
-                    alert(data.message.data)
-                }else{
-                    results = data.message.businesses;
-                    console.log(results);
-                    fill(results);
-                }
-            }
-        },"json");
-    });
+    console.log(localStorage.getItem('data'));
+    var data = JSON.parse(localStorage.getItem('data'));
+    fill(data);
 
     $("#result").on("click",".resultB", function(){
         var id = this.id;
@@ -39,23 +18,26 @@ $(document).ready(function(){
         count++;
     });
 
-    $("#update").on("click",".unresultB", function(){
+    $("#show").on("click",".unresultB", function(){
         console.log(this.id);
         var id = this.id;
         var num= parseInt(id);
         $('#M'+num).remove();
         $('#R'+num).removeClass("hide");
         $('#R'+num).addClass("show");
-        var x = update.indexOf(num);
-        if(x != -1){
-            update.splice(x,1);
-        }
-
+        removeArray(num,update);
     });
 
+    function removeArray(count,array){
+        var x = array.indexOf(count);
+        if(x != -1){
+            array.splice(x,1);
+        }
+
+    }
     function fill(data){
         $('#results').empty();
-        var item = data;
+        var item = data.favourites;
         var panel2 = "";
         var exist = false;
         for(var i = 0; i < item.length;i++){
@@ -65,13 +47,12 @@ $(document).ready(function(){
         $('#results').append(panel2);
     }
 
-    $("#addFav").on("click",function(){
+    $("#removeList").on("click",function(){
         console.log(localStorage.getItem('data'));
         var toSend = JSON.parse(localStorage.getItem('data'));
         console.log(toSend);
         for(var x = 0; x < update.length; x++){
-            console.log(update[x]);
-            toSend.favourites.push(results[update[x]]);
+            removeArray(update[x],toSend.favourites);
         }
         console.log(toSend);
         $.post("https://cloudcompyelp.herokuapp.com/api/update",toSend,function(data,status){
