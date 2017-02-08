140ed902b1726f61633c6a1ba3c2844bc039a4bc
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-08 11:00:51 -0500

    welcome title

--- www/js/access.js
+++ www/js/access.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         console.log("enter");
         var login =JSON.parse(localStorage.getItem('profile'));
         var panel = '<b>Welcome: '+login.Name+ ' </b>' ;
-        $('#intro').append();
+        $('#intro').append(panel);
     }
 });
 
