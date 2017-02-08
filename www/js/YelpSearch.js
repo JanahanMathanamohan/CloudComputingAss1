da893c3d622c1012ce0e47cdcda5d15b1ec87b8e
JanahanMathanamohan <janahan.mathanamohan@uoit.net>
2017-02-08 13:23:30 -0500

    welcome title

--- www/js/YelpSearch.js
+++ www/js/YelpSearch.js
@@ -70,7 +70,6 @@ $(document).ready(function(){
             url:"https://cloudcompyelp.herokuapp.com/api/update",
             type:"POST",
             data:toSend,
-            contentType:"application/json",
             dataType:"json",
             success: function(data,status){
                 if(status == "success"){
